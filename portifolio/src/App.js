c184239763070ea7de8a01fb81a7e4d187faae74
Gabriel Novaes <gabrielnovaisoficial@gmail.com>
2024-04-09 16:58:27 -0300

    09-04

---
+++ portifolio/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import NavBar from './NavBar.js';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <NavBar/>
+      </div>
+      <header className="App-header">
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
